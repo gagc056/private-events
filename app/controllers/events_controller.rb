3a08f3eb908074a40996c806673fe5fd20edadf8
lhscar <gagc056@gmail.com>
2019-12-26 16:46:56 -0300

    creating an invitation controller and invitation model

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
   end
 
   def create
-    @event= Event.new(event_params)
+    @event= current_user.events.build(event_params)
     if @event.save
       flash[:success]= "Event Created"
       redirect_to event_path(@event)
@@ -15,6 +15,7 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
+    redirect_to current_user
   end
 
   def index
@@ -23,6 +24,6 @@ class EventsController < ApplicationController
 
   private
   def event_params
-    params.require(:event).permit(:description)
+    params.require(:event).permit(:description, date)
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+    def new
+      session= Session.new
+
+    end
+
+    def create
+      @user=User.find_by(email: params[:session][:email])
+      if @user
+        sign_in @user
+        redirect_to @user
+      else
+        flash.now[:error]= 'Wrong sign in information'
+        render 'new'
+    end
+
+    def  destroy
+      sign_out if sign_in?
+      redirect_to root_url
+    end
 end
