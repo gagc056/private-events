a11982702cfe1afa54543351ceabddc95d942629
Santiago Guerra <guerrasantiago19@gmail.com>
2020-01-10 12:58:35 -0500

    invitations destroyed

--- app/models/invitation.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Invitation < ApplicationRecord
-  belongs_to :user, class_name: 'Creator', foreign_key: 'creator_id'
-  has_many :invitation
-  belongs_to :event
-  validate :not_attending
-end
